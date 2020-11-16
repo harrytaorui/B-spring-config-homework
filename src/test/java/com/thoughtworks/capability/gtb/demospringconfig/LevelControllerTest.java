e5f1d4f757c8b811b64c108bb6cee7c51e6f9d92
ruitao <harrytaorui@gmail.com>
2020-11-16 14:31:43 +0800

    [taorui]:完成任务

--- src/test/java/com/thoughtworks/capability/gtb/demospringconfig/LevelControllerTest.java
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/LevelControllerTest.java
@@ -1,9 +1,11 @@
 package com.thoughtworks.capability.gtb.demospringconfig;
 
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.ReflectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.is;
@@ -12,15 +14,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("dev")
 public class LevelControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-
-    @Test
-    void should_return_basic_when_level_is_0() throws Exception {
-        mockMvc.perform(get("/level")).andExpect(jsonPath("$", is("basic")));
-    }
 
     @Test
     void should_return_advanced_when_level_is_1() throws Exception {
